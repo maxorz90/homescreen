12e4f85d363738c0199aba2f78b57ad08b5288ed
Maxorz90 <max@maxorz.net>
2024-03-30 00:34:03 +0100

    Improve randomizer

--- js/theme-randomizer.js
+++ js/theme-randomizer.js
@@ -1,27 +1,19 @@
-function createLinkElement(){
-  const theme_filename = getRandomTheme();
+const themes = [ 'maki', '2b' ];
+
+function createLinkElement(theme){
 
   let head = document.getElementsByTagName('HEAD')[0];
   let link = document.createElement('link');
   link.rel = 'stylesheet';
   link.type = 'text/css';
-  link.href = 'css/themes/'+theme_filename;
+  link.href = 'css/themes/'+theme+".css";
   head.appendChild(link);
 }
 
 function getRandomTheme() {
   const number = Math.floor(Math.random() * 2);
 
-  switch (number) {
-    case 0:
-      return "2b.css";
-      break;
-
-    case 1:
-      return "3b.css";
-      break;
-  }
-
+  createLinkElement(themes[number]);
 }
 
-createLinkElement();
+getRandomTheme();
